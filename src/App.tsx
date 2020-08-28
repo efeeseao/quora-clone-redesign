94d62c05a7d9f09fe6587d5f009d73c7b092948e
Fernando dos Santos <nando4803@gmail.com>
2020-08-28 09:17:56 +0100

    This commit import globalstyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { GlobalStyle } from './styles/globalStyles';
+
 function App() {
   return (
     <div>
       <h1>Welcome to Quora</h1>
+      <GlobalStyle />
     </div>
   );
 }
