4d8b94cbe16323ef33936a9d42e39c9be54746a7
Fernando dos Santos <nando4803@gmail.com>
2020-09-19 16:06:11 +0100

    🚀 This commt update leftbar compoent

--- src/components/Leftbar/index.tsx
+++ src/components/Leftbar/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Spaces from './Spaces';
+
+import * as S from './styles';
 
 const Leftbar: React.FC = () => {
-  return <div />;
+  return (
+    <S.Container>
+      <Spaces />
+    </S.Container>
+  );
 };
 
 export default Leftbar;
