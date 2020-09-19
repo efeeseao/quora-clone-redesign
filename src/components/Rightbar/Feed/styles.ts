796b7e29331964dae8a16ba0e27e78a59b1d3a2f
Fernando dos Santos <nando4803@gmail.com>
2020-09-19 19:06:22 +0100

    🚀 This commit update rightbar

--- src/components/Rightbar/Feed/styles.ts
+++ src/components/Rightbar/Feed/styles.ts
@@ -1,20 +1,31 @@
 import styled from 'styled-components';
 import { Check } from '@styled-icons/boxicons-regular';
 
-export const Container = styled.aside``;
+export const Container = styled.aside`
+  margin-top: 2rem;
+  margin-left: 2.625rem;
+`;
 
 export const FeedTitle = styled.span`
-  font-size: 1rem;
+  font-size: 2rem;
+  color: #000;
+  font-weight: 500;
+  padding: 1rem;
+  border-radius: 4px;
+  background: var(--color-border);
 `;
 
 export const FeedMenu = styled.ul`
   list-style: none;
-  margin-top: 8px;
+  margin-top: 1.3rem;
 `;
 
 export const FeedItem = styled.li`
   display: flex;
   align-items: center;
+  padding: 1rem;
+  border-bottom: 1px solid var(--color-separator);
+  background: var(--color-background);
 `;
 
 export const FeedIcon = styled(Check)`
