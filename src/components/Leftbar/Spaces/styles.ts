a52bc050de3f92e35fd384d42cea2f7f9c7b78c6
Fernando dos Santos <nando4803@gmail.com>
2020-09-19 18:52:56 +0100

    🚀 This commit update leftbar

--- src/components/Leftbar/Spaces/styles.ts
+++ src/components/Leftbar/Spaces/styles.ts
@@ -2,17 +2,24 @@ import styled from 'styled-components';
 
 export const Container = styled.aside`
   position: sticky;
-  top: 64px;
+  top: 80px;
+  margin-right: 2.625rem;
 `;
 
 export const Title = styled.span`
   color: var(--color-gray);
-  margin-bottom: 10px;
+  padding: 4px;
+
+  &:hover {
+    background: var(--color-input);
+    opacity: 0.8;
+  }
 `;
 
 export const SpaceImg = styled.img`
-  width: 1rem;
-  height: 1rem;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 8px;
   margin-right: 8px;
 `;
 
@@ -20,5 +27,12 @@ export const SpacesTag = styled.span`
   color: var(--color-gray);
   display: flex;
   align-items: center;
-  padding: 3.5px 0;
+  margin-top: 0.875rem;
+  padding: 0.75rem 0.75rem 0.75rem 0;
+  cursor: pointer;
+
+  &:hover {
+    background: var(--color-border);
+    padding: 0.75rem;
+  }
 `;
