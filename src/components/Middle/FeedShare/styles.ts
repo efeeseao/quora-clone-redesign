2aea3f34d2ad76a811e173b8943056e43c90613e
Fernando dos Santos <nando4803@gmail.com>
2020-09-19 17:03:00 +0100

    🚀 This commt add middle feedShare component

---
+++ src/components/Middle/FeedShare/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  background: var(--color-background);
+`;
+
+export const WriteField = styled.section`
+  display: flex;
+  align-items: center;
+  padding: 1.125rem 1.5rem;
+`;
+
+export const WriteImg = styled.img`
+  width: 1.25rem;
+  height: 1.25rem;
+`;
+
+export const WriteText = styled.span`
+  margin-left: 8px;
+  font-weight: 600;
+`;
