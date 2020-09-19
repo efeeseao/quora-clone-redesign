1ef737075a701ccfac83b8bb9a90877433d0541f
Fernando dos Santos <nando4803@gmail.com>
2020-09-19 19:24:05 +0100

    🚀 This commit add pos styles

--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.section`
+  > h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    color: #000;
+  }
+
+  > p {
+    font-size: 1rem;
+    color: var(--color-black);
+  }
+
+  > img {
+    width: 100%;
+    padding: 1rem;
+  }
+`;
